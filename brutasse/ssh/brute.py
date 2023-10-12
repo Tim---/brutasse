20ffeb0e8750b1946935c86278ba325e9f984935
Timothée Cocault <timothee.cocault@gmail.com>
2023-10-12 23:30:59 +0200

    Generic ssh_brute

--- brutasse/ssh/brute.py
+++ brutasse/ssh/brute.py
@@ -13,7 +13,7 @@ def get_ips(file: TextIO) -> set[str]:
     return {l.strip() for l in file}
 
 
-async def ssh_brute(ip: str):
+async def ssh_brute(ip: str, port: int, username: str, password: str):
     opt1 = {
         'known_hosts': None,
         'preferred_auth': ['keyboard-interactive', 'password'],
@@ -23,9 +23,9 @@ async def ssh_brute(ip: str):
     }
     opt2 = {
         'host': ip,
-        'port': 22,
-        'username': 'admin',
-        'password': 'admin',
+        'port': port,
+        'username': username,
+        'password': password,
         'connect_timeout': 2,
         'login_timeout': 10,
     }
@@ -37,7 +37,8 @@ async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=argparse.FileType('r'))
     args = parser.parse_args()
-    coros = [ssh_brute(ip) for ip in get_ips(args.infile)]
+    coros = [ssh_brute(ip, 22, 'admin', 'admin')
+             for ip in get_ips(args.infile)]
     async for fut in progressbar_execute(coros, 100):
         try:
             res = fut.result()
