94696afda750fa97e96581d7c4192a9219891c14
Timothée Cocault <timothee.cocault@gmail.com>
2023-10-24 23:12:30 +0200

    Add a few SMI messages

--- brutasse/smi/client.py
+++ brutasse/smi/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .proto import SmiStream, Pkt, CapabilitiesReq, CapabilitiesResp
+from .proto import SmiStream, Pkt, CapabilitiesReq, CapabilitiesResp, SelfConfigBackupReq, ConfigBackupReqResp, TlvSeq, TlvLocal, TlvRemote
 
 
 class IbdClient:
@@ -10,9 +10,41 @@ class IbdClient:
     async def get_capabilities(self):
         req = Pkt(version=1, body=CapabilitiesReq(1, 0))
         await self.stream.write_msg(req)
+
         resp = await self.stream.read_msg()
         match resp:
             case Pkt(version=0, body=CapabilitiesResp(1, 0)):
                 pass
             case _:
                 raise Exception(f'Unexpected message {resp}')
+
+    async def backup_local(self):
+        req = Pkt(version=0, body=SelfConfigBackupReq(tlvs=[
+            TlvSeq(1, 0, bytes(6)),
+            TlvLocal('configure tftp-server nvram:startup-config'),
+        ]))
+        await self.stream.write_msg(req)
+
+        # Note: the switch will connect back on port 4786 to send a response
+
+        # TODO: do the tftp get
+
+        req = Pkt(version=0, body=ConfigBackupReqResp(result=1))
+        await self.stream.write_msg(req)
+
+        # Note: the switch will connect back on port 4786 to send a response
+
+    async def backup_remote(self, ip: str):
+        # TODO: listen with tftp server
+
+        req = Pkt(version=0, body=SelfConfigBackupReq(tlvs=[
+            TlvSeq(1, 0, bytes(6)),
+            TlvRemote(
+                'copy system:running-config flash:/config.text',
+                'copy flash:/config.text tftp://{ip}/config.text',
+                '',
+            ),
+        ]))
+        await self.stream.write_msg(req)
+
+        # Note: the switch will connect back on port 4786 to send a response
--- brutasse/smi/proto.py
+++ brutasse/smi/proto.py
@@ -50,6 +50,100 @@ class CapabilitiesReq(Msg, from_=From.IBD_CLI, type_id=4):
         return cls(a, b)
 
 
+class Tlv:
+    repo: dict[int, type['Tlv']] = {}
+    type_id: int
+
+    def __init_subclass__(cls, /, type_id: int, **kwargs: Any):
+        super().__init_subclass__(**kwargs)
+        Tlv.repo[type_id] = cls
+        cls.type_id = type_id
+
+    def build(self) -> bytes:
+        raise NotImplementedError
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        raise NotImplementedError
+
+
+@dataclass
+class TlvSeq(Tlv, type_id=1):
+    seq: int
+    total_fragments: int
+    mac: bytes
+
+    def build(self) -> bytes:
+        return struct.pack('!II6s2x', self.seq, self.total_fragments, self.mac)
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        return cls(*struct.unpack('!II6s2x', raw))
+
+
+@dataclass
+class TlvLocal(Tlv, type_id=2):
+    cmd: str  # configure tftp-server nvram:startup-config
+
+    def build(self) -> bytes:
+        return struct.pack('!336s', self.cmd.encode())
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        cmd, = struct.unpack('!336s', raw)
+        return cls(cmd.rsplit(b'\0').decode())
+
+
+@dataclass
+class TlvRemote(Tlv, type_id=3):
+    cmd1: str  # copy {path}/{hostname}-{mac}.REV2 flash:{hostname}-{mac}.tmp
+    cmd2: str  # copy nvram:startup-config {path}/{hostname}-{mac}.REV2
+    cmd3: str  # copy flash:{hostname}-{mac}.tmp {path}/{hostname}-{mac}.REV1
+
+    def build(self) -> bytes:
+        return struct.pack('!336s336s336s', self.cmd1.encode(), self.cmd2.encode(), self.cmd3.encode())
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        cmd1, cmd2, cmd3 = struct.unpack('!336s336s336s', raw)
+        return cls(cmd1.rsplit(b'\0').decode(), cmd2.rsplit(b'\0').decode(), cmd3.rsplit(b'\0').decode())
+
+
+@dataclass
+class SelfConfigBackupReq(Msg, from_=From.IBD_CLI, type_id=8):
+    tlvs: list[Tlv]
+
+    def build(self) -> bytes:
+        res = b''
+        for tlv in self.tlvs:
+            data = tlv.build()
+            res += struct.pack('!HH', tlv.type_id, len(data) + 4) + data
+        return res
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        tlvs: list[Tlv] = []
+        while len(raw):
+            type_, length = struct.unpack('!HH', raw[:4])
+            data = raw[4:length]
+            tlv = Tlv.repo[type_].parse(data)
+            tlvs.append(tlv)
+            raw = raw[length:]
+        return cls(tlvs)
+
+
+@dataclass
+class ConfigBackupReqResp(Msg, from_=From.IBD_CLI, type_id=9):
+    result: int  # 1 = Success, 2 = Failure
+
+    def build(self) -> bytes:
+        return struct.pack('!I', self.result)
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        return cls(*struct.unpack('!I', raw))
+
+
 # Messages from Client
 
 @dataclass
@@ -62,8 +156,7 @@ class CapabilitiesResp(Msg, from_=From.IBC_SERV, type_id=3):
 
     @classmethod
     def parse(cls, raw: bytes) -> Self:
-        a, b = struct.unpack('!II', raw)
-        return cls(a, b)
+        return cls(*struct.unpack('!II', raw))
 
 
 @dataclass
