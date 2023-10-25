aacaee8e48a37231a503ff61565b24385a8b199a
Timothée Cocault <timothee.cocault@gmail.com>
2023-10-25 20:04:57 +0200

    Add tftp proto

---
+++ brutasse/tftp/proto.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import struct
+from dataclasses import dataclass
+from typing import Self, Any
+
+
+class Msg:
+    repo: dict[int, type['Msg']] = {}
+    type_id: int
+
+    def __init_subclass__(cls, /, type_id: int, **kwargs: Any):
+        super().__init_subclass__(**kwargs)
+        Msg.repo[type_id] = cls
+        cls.type_id = type_id
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        raise NotImplementedError
+
+    def build(self) -> bytes:
+        raise NotImplementedError
+
+
+@dataclass
+class ReadRequest(Msg, type_id=1):
+    filename: str
+    mode: str
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        filename, mode, rest = raw.split(b'\0')
+        assert not rest
+        return cls(filename.decode(), mode.decode())
+
+    def build(self) -> bytes:
+        return f'{self.filename}\0{self.mode}\0'.encode()
+
+
+@dataclass
+class WriteRequest(Msg, type_id=2):
+    filename: str
+    mode: str
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        filename, mode, rest = raw.split(b'\0')
+        assert not rest
+        return cls(filename.decode(), mode.decode())
+
+    def build(self) -> bytes:
+        return f'{self.filename}\0{self.mode}\0'.encode()
+
+
+@dataclass
+class Data(Msg, type_id=3):
+    block_num: int
+    data: bytes
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        block_num, = struct.unpack('!H', raw[:2])
+        return cls(block_num, raw[2:])
+
+    def build(self) -> bytes:
+        return struct.pack('!H', self.block_num) + self.data
+
+
+@dataclass
+class Ack(Msg, type_id=4):
+    block_num: int
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        block_num, = struct.unpack('!H', raw)
+        return cls(block_num)
+
+    def build(self) -> bytes:
+        return struct.pack('!H', self.block_num)
+
+
+@dataclass
+class Error(Msg, type_id=5):
+    code: int
+    msg: str
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        code, = struct.unpack('!H', raw[:2])
+        msg = raw[2:]
+        assert msg[-1] == 0
+        return cls(code, msg[:-1].decode())
+
+    def build(self) -> bytes:
+        return struct.pack('!H', self.code) + f'{self.msg}\0'.encode()
+
+
+@dataclass
+class Pkt:
+    body: Msg
+
+    @classmethod
+    def parse(cls, raw: bytes) -> Self:
+        op, = struct.unpack('!H', raw[:2])
+        body = Msg.repo[op].parse(raw[2:])
+        return cls(body=body)
+
+    def build(self) -> bytes:
+        return struct.pack('!H', self.body.type_id) + self.body.build()
