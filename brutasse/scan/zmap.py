8b0a90ab688befc284be17f94132a034d327420a
Timothée Cocault <timothee.cocault@gmail.com>
2023-11-01 15:32:39 +0100

    Add ztee for zmap buffering

--- brutasse/scan/zmap.py
+++ brutasse/scan/zmap.py
@@ -5,6 +5,7 @@ import asyncio
 from ipaddress import IPv4Address, IPv4Network
 from collections.abc import AsyncGenerator
 from ..utils import get_default_interface, argunparse
+import os
 
 
 async def zmap_scan(options: dict[str, str], ranges: list[IPv4Network]) -> AsyncGenerator[dict[str, str], None]:
@@ -18,13 +19,19 @@ async def zmap_scan(options: dict[str, str], ranges: list[IPv4Network]) -> Async
         'interface': interface
     }
     args = argunparse(base_options | options, map(str, ranges))
-    proc = await asyncio.subprocess.create_subprocess_exec('zmap', *args, stdout=asyncio.subprocess.PIPE)
-    assert proc.stdout
-    async for line in proc.stdout:
+
+    read, write = os.pipe()
+    zmap_proc = await asyncio.subprocess.create_subprocess_exec('zmap', *args, stdout=write)
+    os.close(write)
+    ztee_proc = await asyncio.subprocess.create_subprocess_exec('ztee', '-r', '/dev/null', stdin=read, stdout=asyncio.subprocess.PIPE)
+    os.close(read)
+    assert ztee_proc.stdout
+    async for line in ztee_proc.stdout:
         j = json.loads(line)
         assert isinstance(j, dict)
         yield j
-    await proc.wait()
+    await zmap_proc.wait()
+    await ztee_proc.wait()
 
 
 async def udp_scan(ranges: list[IPv4Network], rate: int, port: int, payload: bytes) -> AsyncGenerator[tuple[IPv4Address, bytes], None]:
