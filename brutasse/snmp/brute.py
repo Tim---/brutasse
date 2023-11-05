d4647c3fbaf5e2ee021c2c54a5a97cf3971e1266
Timothée Cocault <timothee.cocault@gmail.com>
2023-11-05 16:00:27 +0100

    Add progress to snmp brute

--- brutasse/snmp/brute.py
+++ brutasse/snmp/brute.py
@@ -5,13 +5,18 @@ from typing import AsyncIterable
 from ..utils import IPAddress
 from ..scan.ip import udp_scan
 from .proto import make_v2c_request, get_v2c_community
+import progressbar
 
 
 def pkt_gen(ips: list[IPv4Address], communities: list[str]):
-    for community in communities:
-        pkt = make_v2c_request(community)
-        for ip in ips:
-            yield (ip, 161, pkt)
+    with progressbar.ProgressBar(max_value=len(ips) * len(communities), redirect_stdout=True) as bar:
+        i = 0
+        for community in communities:
+            pkt = make_v2c_request(community)
+            for ip in ips:
+                yield (ip, 161, pkt)
+                i += 1
+                bar.update(i)
 
 
 async def brute(ips: list[IPv4Address], communities: list[str]) -> AsyncIterable[tuple[IPAddress, int, str]]:
