a026ca234ada360b62c7a6573351ade3cde2267d
Timothée Cocault <timothee.cocault@gmail.com>
2023-11-10 20:20:41 +0100

    tftp: add dummy server

--- brutasse/tftp/protocol.py
+++ brutasse/tftp/protocol.py
@@ -3,8 +3,8 @@
 import asyncio
 import itertools
 from collections.abc import Iterator
-from anyio.abc import ConnectedUDPSocket
-from .packet import Msg, ReadRequest, WriteRequest, Error, Data, Ack
+from anyio.abc import UDPSocket, ConnectedUDPSocket
+from .packet import Msg, ReadRequest, WriteRequest, Ack, Error, Data
 
 
 class Common:
@@ -99,3 +99,69 @@ class Client(Common):
         resp = await self.send_receive(req)
         self.check_resp_ack(resp, 0)
         await self.send_data(data)
+
+
+Addr = tuple[str, int]
+
+
+class ServerHandler(Common):
+    def __init__(self, server: 'Server', addr: Addr,
+                 queue: asyncio.Queue[bytes]):
+        self.server = server
+        self.addr = addr
+        self.queue = queue
+
+    async def send_msg(self, msg: Msg) -> None:
+        await self.server.send_datagram(msg.build(), self.addr)
+
+    async def recv_msg(self) -> Msg:
+        return Msg.parse(await self.queue.get())
+
+    async def datagram_received(self, data: bytes) -> None:
+        await self.queue.put(data)
+
+    async def run(self) -> None:
+        msg = await self.recv_msg()
+        match msg:
+            case ReadRequest():
+                dummy_data = b'A' * 4096
+                await self.send_data(dummy_data)
+            case WriteRequest():
+                resp = await self.send_receive(Ack(0))
+                await self.recv_data(resp)
+            case _:
+                raise ValueError(f'Unexpected message {msg}')
+
+
+class Server:
+    def __init__(self, udp: UDPSocket):
+        self.udp = udp
+        self.handlers: dict[Addr, ServerHandler] = {}
+        self.queues: dict[Addr, asyncio.Queue[Msg]] = {}
+
+    async def send_datagram(self, data: bytes, addr: Addr) -> None:
+        await self.udp.sendto(data, addr[0], addr[1])
+
+    def remove_handler(self, addr: Addr) -> None:
+        handler = self.handlers.pop(addr)
+        if not handler.queue.empty():
+            # If the client reuses the socket, we may have pending packets
+            # in the queue. Create a new handler with the same queue.
+            self.create_handler(addr, handler.queue)
+
+    def create_handler(self, addr: Addr, queue: asyncio.Queue[bytes]) -> None:
+        handler = ServerHandler(self, addr, queue)
+        task = asyncio.create_task(handler.run())
+        task.add_done_callback(lambda task: self.remove_handler(addr))
+        self.handlers[addr] = handler
+
+    async def datagram_received(self, data: bytes, addr: Addr) -> None:
+        if addr not in self.handlers:
+            queue: asyncio.Queue[bytes] = asyncio.Queue()
+            self.create_handler(addr, queue)
+
+        await self.handlers[addr].datagram_received(data)
+
+    async def run(self) -> None:
+        async for raw, addr in self.udp:
+            await self.datagram_received(raw, addr)
